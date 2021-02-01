ddd48ab8304310185d35a4bc60337f03d232a03f
fentyhall <fentymhall@gmail.com>
2021-02-01 14:48:04 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // form and input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+
+  // ul for new task description
+  const newUl = document.getElementById('tasks');
+
+  // attaching event listeners
+  newTaskForm.addEventListener("submit", (e) => {
+    // stop form from submit
+    e.preventDefault();
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+    
+    newUl.appendChild(newTask);
+    
+    // reset form
+    e.target.reset();
+  })
 });
