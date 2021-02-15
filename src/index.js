15705eeb8eaf967debf3aa74dab023f54202d8bc
fentyhall <fentymhall@gmail.com>
2021-02-14 21:17:14 -0800

    update solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
+// form and input fields
+const newTaskForm = document.getElementById("create-task-form");
 
-  // form and input fields
-  const newTaskForm = document.getElementById("create-task-form");
+// ul for new task description
+const newUl = document.getElementById('tasks');
+
+// attaching event listeners
+newTaskForm.addEventListener("submit", function(e) {
+  // stop form from submit
+  e.preventDefault();
   const newTaskDescription = document.getElementById("new-task-description");
 
-  // ul for new task description
-  const newUl = document.getElementById('tasks');
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  newUl.appendChild(newTask);
+  
+  const newButton = document.createElement("button");
+  newButton.class = "delete-btn";
+  newButton.innerText = "X";
+  newTask.appendChild(newButton);
+  
+  // reset form
+  e.target.reset();
+})
 
-  // attaching event listeners
-  newTaskForm.addEventListener("submit", (e) => {
-    // stop form from submit
-    e.preventDefault();
-    const newTask = document.createElement("li");
-    newTask.innerText = newTaskDescription.value;
-    
-    newUl.appendChild(newTask);
-    
-    // reset form
-    e.target.reset();
-  })
-});
+newUl.addEventListener("click", function(event) {
+  if (event.target.class === "delete-btn") {
+    event.target.parentElement.remove();
+  }
+})
